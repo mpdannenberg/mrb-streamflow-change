98f6ceceb759974c901d67408368987d504862c8
mpdannenberg <matthew-dannenberg@uiowa.edu>
2023-09-14 15:51:21 -0700

    Rescale modeled streamflow to match observed mean and variance (but save the unscaled as well)

--- model_mstmip_contributions_runoff.m
+++ model_mstmip_contributions_runoff.m
@@ -108,7 +108,7 @@ for k = 1:nk
 end
 
 %% Calculate water balance using CRU precip and MsTMIP ET
-cd('D:\Publications\NSF_LMRB_CFR')
+cd('D:\Publications\Dannenberg_et_al_MRB_streamflow_change')
 Ptot = sum(repmat(area(MRBidx)', nt, 1) .* P(:,MRBidx), 2);
 Pmean = sum(repmat(area(MRBidx)', nt, 1) .* repmat(mean(P(1:30,MRBidx),'omitnan'),nt,1), 2);
 ETtot_RG1 = NaN(nt, nk);
@@ -138,126 +138,42 @@ WB_SG1 = (repmat(Ptot,1,nk) - ETtot_SG1) * (1/365) * (1/24) * (1/60) * (1/60);
 WB_SG2 = (repmat(Ptot,1,nk) - ETtot_SG2) * (1/365) * (1/24) * (1/60) * (1/60);
 WB_SG3 = (repmat(Ptot,1,nk) - ETtot_SG3) * (1/365) * (1/24) * (1/60) * (1/60);
 
-% %% scale to match mean/variance of observed streamflow
-% WB_RG1_sc = NaN(size(WB_RG1));
-% WB_SG1_sc = NaN(size(WB_SG1));
-% WB_SG2_sc = NaN(size(WB_SG2));
-% WB_SG3_sc = NaN(size(WB_SG3));
-% for k = 1:nk
-%     wb = WB_SG3(:,k);
-%     [~,ia,ib] = intersect(syear:eyear, T.Year);
-%     
-%     x = wb(ia); y = T.HEMO(ib);
-%     mx = mean(x(~isnan(x) & ~isnan(y)));
-%     my = mean(y(~isnan(x) & ~isnan(y)));
-%     sx = std(x(~isnan(x) & ~isnan(y)));
-%     sy = std(y(~isnan(x) & ~isnan(y)));
-% 
-%     WB_RG1_sc(:,k) = my + sy*((WB_RG1(:,k) - mx)/sx);
-%     WB_SG1_sc(:,k) = my + sy*((WB_SG1(:,k) - mx)/sx);
-%     WB_SG2_sc(:,k) = my + sy*((WB_SG2(:,k) - mx)/sx);
-%     WB_SG3_sc(:,k) = my + sy*((WB_SG3(:,k) - mx)/sx);
-%     
-% %     mdl = fitlm(wb(ia), T.HEMO(ib));
-% %     mdl.predict
-% end
-% WB_RG1 = WB_RG1_sc;
-% WB_SG1 = WB_SG1_sc;
-% WB_SG2 = WB_SG2_sc;
-% WB_SG3 = WB_SG3_sc;
+%% scale to match mean/variance of observed streamflow
+WB_RG1_unscaled = WB_RG1;
+WB_SG1_unscaled = WB_SG1;
+WB_SG2_unscaled = WB_SG2;
+WB_SG3_unscaled = WB_SG3;
+WB_RG1_sc = NaN(size(WB_RG1));
+WB_SG1_sc = NaN(size(WB_SG1));
+WB_SG2_sc = NaN(size(WB_SG2));
+WB_SG3_sc = NaN(size(WB_SG3));
+for k = 1:nk
+    wb = WB_SG3(:,k);
+    [~,ia,ib] = intersect(syear:eyear, T.Year);
+    
+    x = wb(ia); y = T.HEMO(ib);
+    mx = mean(x(~isnan(x) & ~isnan(y)));
+    my = mean(y(~isnan(x) & ~isnan(y)));
+    sx = std(x(~isnan(x) & ~isnan(y)));
+    sy = std(y(~isnan(x) & ~isnan(y)));
+
+    WB_RG1_sc(:,k) = my + sy*((WB_RG1(:,k) - mx)/sx);
+    WB_SG1_sc(:,k) = my + sy*((WB_SG1(:,k) - mx)/sx);
+    WB_SG2_sc(:,k) = my + sy*((WB_SG2(:,k) - mx)/sx);
+    WB_SG3_sc(:,k) = my + sy*((WB_SG3(:,k) - mx)/sx);
+    
+end
+WB_RG1 = WB_RG1_sc;
+WB_SG1 = WB_SG1_sc;
+WB_SG2 = WB_SG2_sc;
+WB_SG3 = WB_SG3_sc;
 
 %% Save data
 year = syear:eyear;
 lat = lat(latidx);
 lon = lon(lonidx);
 save('./data/MsTMIP_WaterBudget.mat', 'year', 'lat', 'lon', 'models',...
-    'ET_RG1','ET_SG1','ET_SG2','ET_SG3','WB_RG1','WB_SG1','WB_SG2','WB_SG3', 'P', '-v7.3');
+    'ET_RG1','ET_SG1','ET_SG2','ET_SG3','WB_RG1','WB_SG1','WB_SG2','WB_SG3',...
+    'WB_RG1_unscaled','WB_SG1_unscaled','WB_SG2_unscaled','WB_SG3_unscaled','P', '-v7.3');
 
-% %% make figures
-% h = figure('Color','w');
-% h.Units = 'inches';
-% h.Position = [1 1 5 7];
-% 
-% subplot(3,1,1)
-% plot(repmat([syear:eyear]', 1, nk), WB_SG1)
-% set(gca, 'XTickLabel','', 'TickDir','out', 'XLim',[syear eyear], 'YLim',[0 15000])
-% box off;
-% hold on;
-% plot(T.Year, T.HEMO, 'k-', 'LineWidth',1.5)
-% title('SG1')
-% 
-% subplot(3,1,2)
-% plot(repmat([syear:eyear]', 1, nk), WB_SG2)
-% set(gca, 'XTickLabel','', 'TickDir','out', 'XLim',[syear eyear], 'YLim',[0 15000])
-% box off;
-% hold on;
-% plot(T.Year, T.HEMO, 'k-', 'LineWidth',1.5)
-% title('SG2')
-% ylabel('Streamflow (m^{3} s^{-1})', 'FontSize',11)
-% 
-% subplot(3,1,3)
-% plot(repmat([syear:eyear]', 1, nk), WB_SG3)
-% set(gca, 'TickDir','out', 'XLim',[syear eyear], 'YLim',[0 15000])
-% box off;
-% hold on;
-% plot(T.Year, T.HEMO, 'k-', 'LineWidth',1.5)
-% title('SG3')
-% 
-% %% make figure showing unscaled and rescaled WB vs. HEMO flow
-% h = figure('Color','w');
-% h.Units = 'inches';
-% h.Position = [1 1 5 6];
-% 
-% subplot(2,1,1)
-% plot(syear:eyear, WB_SG3)
-% set(gca, 'XTickLabel','', 'TickDir','out', 'XLim',[syear eyear], 'YLim',[0 15000])
-% box off;
-% hold on;
-% plot(T.Year, T.HEMO, 'k-', 'LineWidth',1.5)
-% title('unscaled')
-% hold off;
-% ylabel('Streamflow (m^{3} s^{-1})', 'FontSize',11)
-% 
-% subplot(2,1,2)
-% plot(syear:eyear, WB_SG3_sc)
-% set(gca, 'XTickLabel','', 'TickDir','out', 'XLim',[syear eyear], 'YLim',[0 6000])
-% box off;
-% hold on;
-% plot(T.Year, T.HEMO, 'k-', 'LineWidth',1.5)
-% title('scaled')
-% ylabel('Streamflow (m^{3} s^{-1})', 'FontSize',11)
-% 
-% 
-% %% plot change due to each factor
-% h = figure('Color','w');
-% h.Units = 'inches';
-% h.Position = [1 1 5 7];
-% 
-% subplot(3,1,1)
-% plot(repmat([syear:eyear]', 1, nk), WB_SG1 - WB_RG1)
-% set(gca, 'XTickLabel','', 'TickDir','out', 'XLim',[syear eyear], 'YLim',[-4000 4000])
-% box off;
-% hold on;
-% % plot(T.Year, T.HEMO, 'k-', 'LineWidth',1.5)
-% title('Climate')
-% plot(syear:eyear, median(WB_SG1 - WB_RG1, 2),'k-','LineWidth',1.5)
-% 
-% subplot(3,1,2)
-% plot(repmat([syear:eyear]', 1, nk), WB_SG2 - WB_SG1)
-% set(gca, 'XTickLabel','', 'TickDir','out', 'XLim',[syear eyear], 'YLim',[-4000 4000])
-% box off;
-% hold on;
-% % plot(T.Year, T.HEMO, 'k-', 'LineWidth',1.5)
-% title('LULCC')
-% ylabel('Streamflow (m^{3} s^{-1})', 'FontSize',11)
-% plot(syear:eyear, median(WB_SG2 - WB_SG1, 2),'k-','LineWidth',1.5)
-% 
-% subplot(3,1,3)
-% plot(repmat([syear:eyear]', 1, nk), WB_SG3 - WB_SG2)
-% set(gca, 'TickDir','out', 'XLim',[syear eyear], 'YLim',[-4000 4000])
-% box off;
-% hold on;
-% % plot(T.Year, T.HEMO, 'k-', 'LineWidth',1.5)
-% title('CO2')
-% plot(syear:eyear, median(WB_SG3 - WB_SG2, 2),'k-','LineWidth',1.5)
 
