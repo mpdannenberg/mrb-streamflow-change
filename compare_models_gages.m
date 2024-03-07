aa947ac5de1a61bd63cd8aa11d6b81cf84178457
mpdannenberg <matthew-dannenberg@uiowa.edu>
2024-03-06 18:38:53 -0600

    remove scaled panel

--- compare_models_gages.m
+++ compare_models_gages.m
@@ -8,7 +8,7 @@ basins = shaperead('./data/LMRBgagebasins/lmrb_shapefiles.shp');
 %% Make figure showing comparison between gage data and MsTMIP
 h = figure('Color','w');
 h.Units = 'inches';
-h.Position = [1 1 6.5 3.75];
+h.Position = [1 1 6.5 3.];
 
 load ./data/MsTMIP_WaterBudget;
 [~, ia, ib] = intersect(year, T.Year);
@@ -16,7 +16,7 @@ load ./data/MsTMIP_WaterBudget;
 % HEMO - unscaled
 Q = WB_SG3_unscaled;
 
-subplot(3,4,[1 3])
+subplot(2,4,[1 3])
 plot(year(ia)', Q(ia,:), '-', 'Color',[0.8 0.8 0.8])
 hold on;
 plot(year(ia)', mean(Q(ia,:), 2), '-', 'Color',[0.5 0.5 0.5], 'LineWidth',1.5)
@@ -27,7 +27,7 @@ ylabel('Q (m^{3} s^{-1})')
 ylim = get(gca, 'YLim');
 text(1931, ylim(2), 'a', 'FontSize',12, 'VerticalAlignment','middle')
 
-subplot(3,4,4)
+subplot(2,4,4)
 lim = [0 10500];
 plot(lim, lim, '-', 'Color',[0.5 0.5 0.5])
 hold on;
@@ -42,41 +42,11 @@ r = corr(T.HEMO(ib), mean(Q(ia,:), 2), 'rows','pairwise');
 text(lim(2), lim(1), sprintf('R = %.02f', r), 'HorizontalAlignment','right', 'VerticalAlignment','bottom', 'FontSize',7)
 text(diff(lim)*0.04, lim(2), 'b', 'FontSize',12, 'VerticalAlignment','middle')
 
-% HEMO - scaled
-Q = WB_SG3;
-
-subplot(3,4,[5 7])
-plot(year(ia)', Q(ia,:), '-', 'Color',[0.8 0.8 0.8])
-hold on;
-plot(year(ia)', mean(Q(ia,:), 2), '-', 'Color',[0.5 0.5 0.5], 'LineWidth',1.5)
-plot(T.Year(ib), T.HEMO(ib), 'k-', 'LineWidth',1.5)
-set(gca, 'TickDir','out', 'XTickLabel','')
-box off;
-ylabel('Q (m^{3} s^{-1})')
-ylim = get(gca, 'YLim');
-text(1931, ylim(2), 'c', 'FontSize',12, 'VerticalAlignment','middle')
-
-subplot(3,4,8)
-lim = [0 6000];
-plot(lim, lim, '-', 'Color',[0.5 0.5 0.5])
-hold on;
-scatter(T.HEMO(ib), mean(Q(ia,:), 2), 10, "black","filled")
-set(gca, 'TickDir','out', 'YLim',lim, 'XLim',lim, 'TickLength',[0.04 0])
-box off;
-ax = gca;
-ax.Position(1) = 0.82;
-ax.Position(3) = 0.12;
-ylabel('MsTMIP')
-r = corr(T.HEMO(ib), mean(Q(ia,:), 2), 'rows','pairwise');
-text(lim(2), lim(1), sprintf('R = %.02f', r), 'HorizontalAlignment','right', 'VerticalAlignment','bottom', 'FontSize',7)
-text(diff(lim)*0.04, lim(2), 'd', 'FontSize',12, 'VerticalAlignment','middle')
-
-% WBM
 load ./data/McCabeWilliams_WaterBudget.mat;
 [~, ia, ib] = intersect(year, T.Year);
 Q = Q_S3_WY;
 
-subplot(3,4,[9 11])
+subplot(2,4,[5 7])
 p1 = plot(year(ia)', Q(ia), '-', 'Color',[0.5 0.5 0.5], 'LineWidth',1.5);
 hold on;
 plot(T.Year(ib), T.HEMO(ib), 'k-', 'LineWidth',1.5)
@@ -84,9 +54,9 @@ set(gca, 'TickDir','out')
 box off;
 ylabel('Q (m^{3} s^{-1})')
 ylim = get(gca, 'YLim');
-text(1931, ylim(2), 'e', 'FontSize',12, 'VerticalAlignment','middle')
+text(1931, ylim(2), 'c', 'FontSize',12, 'VerticalAlignment','middle')
 
-subplot(3,4,12)
+subplot(2,4,8)
 lim = [0 8000];
 plot(lim, lim, '-', 'Color',[0.5 0.5 0.5])
 hold on;
@@ -97,10 +67,11 @@ ax = gca;
 ax.Position(1) = 0.82;
 ax.Position(3) = 0.12;
 ylabel('WBM')
-xlabel('Gage')
+xl = xlabel('Gage');
+xl.Position(2) = -1500;
 r = corr(T.HEMO(ib), mean(Q(ia,:), 2), 'rows','pairwise');
 text(lim(2), lim(1), sprintf('R = %.02f', r), 'HorizontalAlignment','right', 'VerticalAlignment','bottom', 'FontSize',7)
-text(diff(lim)*0.04, lim(2), 'f', 'FontSize',12, 'VerticalAlignment','middle')
+text(diff(lim)*0.04, lim(2), 'd', 'FontSize',12, 'VerticalAlignment','middle')
 
 set(gcf,'PaperPositionMode','auto')
 print('-dtiff','-f1','-r600','./output/models-vs-gages.tif')
